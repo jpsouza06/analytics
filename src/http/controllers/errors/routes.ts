062043784244486562ead7da5d7944d60dd69bb4
João Paulo <joaopaulosc06@gmail.com>
2023-10-18 21:02:13 -0300

    add swagger

--- src/http/controllers/errors/routes.ts
+++ src/http/controllers/errors/routes.ts
@@ -147,5 +147,117 @@ export async function errorRoutes(app: FastifyInstance) {
       handler: Get
    })
 
-   app.post('/error/query/:page', Query)
+   app.post('/error/query/:page', {
+      schema: {
+         tags: ["Errors"],
+         params: {
+            type: 'object',
+            properties: {
+               page: {
+                  type: 'string'
+               }
+            }
+         },
+         body: {
+            type: "object",
+            properties: {
+               unit: { type: "string" },
+               rotina: { type: "string" },
+               modulo: { type: "string" },
+               conteudo: { type: "string" },
+               dataInicio: { type: "string", format: "date" },
+               dataFim: { type: "string", format: "date" },
+               orderBy: {
+                  type: "object",
+                  description:
+                     "Os campos do orderBy devem ser passados com 'asc'(ordem crescente) ou 'desc'(ordem decrescente)",
+                  properties: {
+                     unit: {
+                        type: "string",
+                        enum: ["asc", "desc"],
+                     },
+                     rotina: {
+                        type: "string",
+                        enum: ["asc", "desc"]
+                     },
+                     modulo: {
+                        type: "string",
+                        enum: ["asc", "desc"]
+                     },
+                     conteudo: {
+                        type: "string",
+                        enum: ["asc", "desc"]
+                     },
+                     createdAt: {
+                        type: "string",
+                        enum: ["asc", "desc"]
+                     },
+                  }
+               }
+            }
+
+         },
+         response: {
+            200: {
+               description: 'Sucesso',
+               type: "object",
+               properties: {
+                  errors: {
+                     type: 'array',
+                     properties: {
+                        error: {
+                           type: "object",
+                           properties: {
+                              id: { type: "string" },
+                              unit: { type: "string" },
+                              rotina: { type: "string" },
+                              modulo: { type: "string" },
+                              conteudo: { type: "string" },
+                              createdAt: { type: "string" }
+                           }
+                        }
+                     },
+                     example: [
+                        {
+                           "id": "267915cb-921f-4356-872f-203f42d973b8",
+                           "unit": "Unit",
+                           "rotina": "Rotina",
+                           "modulo": "Modulo",
+                           "conteudo": "Conteudo",
+                           "createdAt": "2023-10-17T00:48:08.507Z"
+                        }
+                     ]
+                  }
+               },
+            },
+            400: {
+               description: 'Requisição incorreta',
+               type: 'object',
+               properties: {
+                  message: {
+                     type: 'object',
+                     example: [
+                        {
+                           "validation": "uuid",
+                           "code": "invalid_string",
+                           "message": "Invalid uuid",
+                           "path": [
+                              "errorId"
+                           ]
+                        }
+                     ]
+                  }
+               }
+            },
+            500: {
+               description: 'Erro interno do servidor',
+               type: 'object',
+               properties: {
+                  message: { type: 'string' }
+               }
+            },
+         },
+      },
+      handler: Query
+   })
 }
