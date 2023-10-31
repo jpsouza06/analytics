c004bfc37dd6f0af1f407b6d1716738aa2b9712f
João Paulo <joaopaulosc06@gmail.com>
2023-10-31 13:40:28 -0300

    fix bug in prisma model

---
+++ prisma/migrations/20231031163932_/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "pageViews" ALTER COLUMN "codCliente" SET DEFAULT '';
+
+-- AlterTable
+ALTER TABLE "systemStarted" ALTER COLUMN "cidade" SET DEFAULT '',
+ALTER COLUMN "versao" SET DEFAULT '';
